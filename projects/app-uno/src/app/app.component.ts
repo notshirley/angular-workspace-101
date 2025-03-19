a1dbf9f32b05b641408c2cef45c5f0af22e405b7
SWu <SWu@nbme.org>
2025-03-19 14:23:24 -0400

    add a random text service

--- projects/app-uno/src/app/app.component.ts
+++ projects/app-uno/src/app/app.component.ts
@@ -1,14 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { BackgroundChangerComponent } from "../../../the-library/src/background-changer/background-changer.component";
+import { BackgroundChangerService } from '../../../the-library/src/public-api';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, BackgroundChangerComponent],
+  imports: [BackgroundChangerComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'app-uno';
+  text = ""
+  
+  constructor(private service: BackgroundChangerService) {
+    this.service.getLoremIpsum().subscribe((result) => this.text = result);
+  }
+
 }
--- projects/the-library/src/background-changer/background-changer.service.ts
+++ projects/the-library/src/background-changer/background-changer.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class BackgroundChangerService {
+  private apiUrl = 'https://api.api-ninjas.com/v1/loremipsum?paragraphs=3';
+  private apiKey = 'c5ZVHgdbWliBXrvbkp9nwg==4z7IQewx1fYLL6ej';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getLoremIpsum(): Observable<string> {
+    return this.http
+      .get<{ text: string }>(this.apiUrl, {
+        headers: { 'X-Api-Key': this.apiKey },
+      })
+      .pipe(
+        map((response) => {
+          console.log(response.text)
+          return response.text;
+        })
+      );
+  }
 }
